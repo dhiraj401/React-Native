12c9ad7ed88c5ec96e6138d50f04e249f8bde1f7
Dhiraj Kumar Tiwari <dktiwari401@gmai.com>
2025-01-18 14:15:50 -0800

    View,Button,Text

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30, backgroundColor:""}}>App</Text>
+      <Text style={{fontSize: 30}}>writen by Dhiraj Tiwari</Text>
+      <Button title="Press Here" />
+    </View>
+  );
+};
+
+export default App;
